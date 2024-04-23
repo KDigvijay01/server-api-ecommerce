7156965c255d0b152522bfc4c28b4c4d03571675
Digvijay Singh <digvijaysingh@Digvijays-MacBook-Air.local>
2024-04-23 19:32:18 +0530

    del in default

--- default.js
+++ default.js
@@ -3,14 +3,32 @@ import { products } from "./constants/data.js";
 import Product from "./model/product-schema.js";
 
 
+async function deleteDocumentById(id) {
+    try {
+        const deletedDocument = await Product.findByIdAndDelete(id);
+        if (deletedDocument) {
+            console.log(`Document with ID ${id} deleted successfully.`);
+        } else {
+            console.log(`No document found with ID ${id}.`);
+        }
+    } catch (error) {
+        console.error("Error while deleting document:", error);
+    }
+}
+
+
+
 const DefaultData=async()=>{
     try{
-        // const products = await Product.find({}); // Find all documents to delete
+        const productsArray = await Product.find({}); // Find all documents to delete
         // for (const product of products) {
         //     await product.deleteOne(); // Delete each document individually
         // }
-
-        const result = await Product.collection.deleteMany({});
+        if(productsArray.length){
+             for (const product of productsArray) {
+                    await deleteDocumentById(product._id);
+        } 
+        }
         
         await Product.insertMany(products);
         
