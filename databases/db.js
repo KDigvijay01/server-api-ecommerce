15c10ae7b2f596c70a8e53327c773ab376d7ec82
Digvijay Singh <digvijaysingh@Digvijays-MacBook-Air.local>
2024-04-23 08:52:17 +0530

    remove old payment files

--- databases/db.js
+++ databases/db.js
@@ -1,11 +1,10 @@
-
+import dotenv from "dotenv";
 import mongoose from "mongoose";
-import * as KEYS from "../config/keys.js"
 
+dotenv.config();
 
 export const Connection=async()=>{
-    console.log("keys", KEYS.default.default.USERNAME)
-    const URL=`mongodb+srv://${KEYS.default.default.USERNAME}:${KEYS.default.default.PASSWORD}@ecomcluster.fnhso0c.mongodb.net/?retryWrites=true&w=majority`;
+    const URL=`${process.env.MONGO_URL}/?retryWrites=true&w=majority`;
     try{
         await mongoose.connect(URL, {useUnifiedTopology: true, useNewUrlParser: true})
         console.log("Database connected successfully");
